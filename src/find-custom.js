3a72d4317220b7244e429b8baaa3618ed9ab064e
Bui Van Giang <it82015@gmail.com>
2021-10-13 22:13:48 +0700

    test chua lam xong

--- src/find-custom.js
+++ src/find-custom.js
@@ -1,5 +1,9 @@
 const findCustom = (func, arr) => {
     // write code in here
+    if (func ==true && (arr.length-1) >0){
+        return (arr.length-1);
+    }
+    else return findCustom(func,arr); //testchua lam xong
 };
 
 module.exports = findCustom;
